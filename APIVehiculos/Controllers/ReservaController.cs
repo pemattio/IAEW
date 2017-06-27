537ed1de07ea090a4983054b17877036b2b12ab4
Paolo Emilio Mattio <pemattio@gmail.com>
2017-06-27 14:43:39 -0300

    Registrar reserva

--- APIVehiculos/Controllers/ReservaController.cs
+++ APIVehiculos/Controllers/ReservaController.cs
@@ -33,34 +33,50 @@ namespace APIVehiculos.Controllers
             return Reservas.Reservas;
         }
 
-        [Route("api/Reserva/CancelarReserva/{idReserva}")]
-        [HttpGet]
-        public void CancelarReserva(string idReserva)
+        [Route("api/Reservas/CancelarReserva/{idReserva}")]
+        [HttpPost]
+        public IHttpActionResult CancelarReserva(string idReserva)
         {
-
-            var cliente = new WCF.WCFReservaVehiculosClient();
-            CancelarReservaRequest res = new CancelarReservaRequest { CodigoReserva = idReserva };
-            CancelarReservaResponse Reservas = cliente.CancelarReserva(res);
-
+            try
+            {
+                var cliente = new WCF.WCFReservaVehiculosClient();
+                CancelarReservaRequest res = new CancelarReservaRequest { CodigoReserva = idReserva };
+                CancelarReservaResponse Reservas = cliente.CancelarReserva(res);
+                return Ok(Reservas);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
         }
 
-        [Route("api/Reserva/ReservarVehiculo")]
-        [HttpGet]
-        public void ReservarVehiculo([FromBody]ReservaEntity reserva)
+        [Route("api/Reservas/ReservarVehiculo")]
+        [HttpPost]
+        public IHttpActionResult ReservarVehiculo([FromBody]ReservaEntity reserva)
         {
+            try
+            {
+                if (reserva == null)
+                    return BadRequest();
 
-            var cliente = new WCF.WCFReservaVehiculosClient();
-            ReservarVehiculoRequest res = new ReservarVehiculoRequest { 
-                ApellidoNombreCliente = reserva.ApellidoNombreCliente, 
-                FechaHoraDevolucion = reserva.FechaHoraDevolucion,
-                FechaHoraRetiro = reserva.FechaHoraRetiro,
-                IdVehiculoCiudad = reserva.VehiculoPorCiudadId,
-                LugarDevolucion = lugarRetiroDevolucion(reserva.LugarDevolucion),
-                LugarRetiro = lugarRetiroDevolucion(reserva.LugarRetiro),
-                NroDocumentoCliente = reserva.NroDocumentoCliente
-            };
-            ReservarVehiculoResponse Reservas = cliente.ReservarVehiculo(res);
-
+                var cliente = new WCF.WCFReservaVehiculosClient();
+                ReservarVehiculoRequest res = new ReservarVehiculoRequest
+                {
+                    ApellidoNombreCliente = reserva.ApellidoNombreCliente,
+                    FechaHoraDevolucion = reserva.FechaHoraDevolucion,
+                    FechaHoraRetiro = reserva.FechaHoraRetiro,
+                    IdVehiculoCiudad = reserva.VehiculoPorCiudadId,
+                    LugarDevolucion = lugarRetiroDevolucion(reserva.LugarDevolucion),
+                    LugarRetiro = lugarRetiroDevolucion(reserva.LugarRetiro),
+                    NroDocumentoCliente = reserva.NroDocumentoCliente
+                };
+                ReservarVehiculoResponse Reservas = cliente.ReservarVehiculo(res);
+                return Ok(Reservas);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
         }
 
         private LugarRetiroDevolucion lugarRetiroDevolucion(string lugarRetiroDevolucion)
@@ -70,9 +86,9 @@ namespace APIVehiculos.Controllers
                 case "0":
                     return LugarRetiroDevolucion.Aeropuerto;
                 case "1":
-                    return LugarRetiroDevolucion.Hotel;
-                case "2":
                     return LugarRetiroDevolucion.TerminalBuses;
+                case "2":
+                    return LugarRetiroDevolucion.Hotel;
                 default:
                     return new LugarRetiroDevolucion();
             }
