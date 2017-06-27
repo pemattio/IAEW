f38534a2485ad96d3e18cab1d853a2f7d0efadfe
Paolo Emilio Mattio <pemattio@gmail.com>
2017-06-27 11:13:29 -0300

    Agregadas constantes

---
+++ Constants/Clients.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    public static class Clients
+    {
+        public readonly static Client Client1 = new Client
+        {
+            Id = "TPI_GrupoNro5",
+            Secret = "pass12345",
+            RedirectUrl = Paths.AuthorizeCodeCallBackPath
+        };
+    }
+
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+        public string RedirectUrl { get; set; }
+    }
+}
---
+++ Constants/Paths.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    public static class Paths
+    {
+        /// <summary>
+        /// AuthorizationServer project should run on this URL
+        /// </summary>
+        public const string AuthorizationServerBaseAddress = "http://104.197.29.243:8080/openam";
+
+        /// <summary>
+        /// ResourceServer project should run on this URL
+        /// </summary>
+        public const string ResourceServerBaseAddress = "http://localhost:61319";
+
+        /// <summary>
+        /// AuthorizationCodeGrant project should be running on this URL.
+        /// </summary>
+        public const string AuthorizeCodeCallBackPath = "http://localhost:54705/Home/callBack";
+
+        public const string AuthorizePath = "/OAuth/Authorize";
+        public const string TokenPath = "/OAuth/Token";
+        public const string LoginPath = "/Account/Login";
+        public const string LogoutPath = "/Account/Logout";
+        public const string MePath = "/api/Me";
+    }
+}
